cd43dc2e043767a75750592c000aac0763204c61
Wilson Chan <wilson.chan@industrieit.com>
2020-02-14 10:31:34 +0800

    add configuration

--- src/main/java/com/rad/kit/dsappsvc/SimpleWebSecurityConfiguration.java
+++ src/main/java/com/rad/kit/dsappsvc/SimpleWebSecurityConfiguration.java
@@ -2,10 +2,12 @@ package com.rad.kit.dsappsvc;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @EnableWebSecurity
 public class SimpleWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
